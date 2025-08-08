557deb341ff9dd66b1ae5e321cda043a5b94d254
Angane2004 <anganetanveer@gmail.com>
2025-08-08 16:16:55 +0530

    backend issue fixed

--- server/index.js
+++ server/index.js
@@ -16,6 +16,8 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Serve uploaded files
@@ -29,6 +31,13 @@ app.use('/api/analysis', analysisRoutes);
 app.get('/api', (req, res) => {
   res.send('API route is working!');
 });
+
+// The "catchall" handler: for any request that doesn't match a specific API route,
+// send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -38,17 +47,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-// 404 handler
-app.use('*', (req, res) => {
-  res.status(404).json({ error: 'Route not found' });
-});
-
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
   console.log(`📁 API available at http://localhost:${PORT}/api`);
-}); 
-
-// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
